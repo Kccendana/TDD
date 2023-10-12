cff5032d02c483d10cfbb1352ebfd5e7ecb4909a
kcccendana <kc_telyn@yahoo.com>
2023-10-13 00:52:57 +0900

    Update solver_spec.rb

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,9 +4,17 @@ require_relative '../solver'
 describe Solver do
   solver = Solver.new
 
-  context '#factorial' do
-    it 'returns the factorial of the number' do
-      expect(solver.factorial(5)).to eql(120)
+  describe '#factorial' do
+    it 'returns 1 for factorial of 0' do
+      expect(solver.factorial(0)).to eq(1)
+    end
+
+    it 'returns the correct factorial for a positive integer' do
+      expect(solver.factorial(5)).to eq(120)
+    end
+
+    it 'raises an exception for a negative integer' do
+      expect { solver.factorial(-3) }.to raise_error(ArgumentError)
     end
   end
 
@@ -23,18 +31,18 @@ describe Solver do
   describe '#fizzbuzz' do
     solve = Solver.new
     it 'returns fizz if N is divisible by 3' do
-      expect(solve.fizzbuzz(3)).to eql 'fizz'
+      expect(solver.fizzbuzz(3)).to eql 'fizz'
     end
     it 'returns buzz if N is divisible by 5' do
-      expect(solve.fizzbuzz(5)).to eql 'buzz'
+      expect(solver.fizzbuzz(5)).to eql 'buzz'
     end
 
     it 'returns fizzbuzz if N is divisible by 5 and 3' do
-      expect(solve.fizzbuzz(15)).to eql 'fizzbuzz'
+      expect(solver.fizzbuzz(15)).to eql 'fizzbuzz'
     end
 
     it 'returns string of N if not divisible by 5 and 3' do
-      expect(solve.fizzbuzz(7)).to eql '7'
+      expect(solver.fizzbuzz(7)).to eql '7'
     end
   end
 end
