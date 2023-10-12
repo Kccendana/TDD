57610a2c64c38fcf417b443d012b6cfc432f403c
kcccendana <kc_telyn@yahoo.com>
2023-10-13 00:12:35 +0900

    create test for fizzbuzz

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -19,4 +19,22 @@ describe Solver do
       expect(solver.reverse('')).to eq('')
     end
   end
+
+  describe '#fizzbuzz' do
+    solve = Solver.new
+    it 'returns fizz if N is divisible by 3' do
+      expect(solve.fizzbuzz(3)).to eql 'fizz'
+    end
+    it 'returns buzz if N is divisible by 5' do
+      expect(solve.fizzbuzz(5)).to eql 'buzz'
+    end
+
+    it 'returns fizzbuzz if N is divisible by 5 and 3' do
+      expect(solve.fizzbuzz(15)).to eql 'fizzbuzz'
+    end
+
+    it 'returns string of N if not divisible by 5 and 3' do
+      expect(solve.fizzbuzz(7)).to eql '7'
+    end
+  end
 end
